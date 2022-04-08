ca55d7a26ba513d61f05f1d5a61cfcd44041a2f0
Ken Steckler <steckler92ken@gmail.com>
2022-04-07 17:55:11 -0700

    update

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const Gift = require('./models/gift');
-const Group = require('./models/group');
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-const userRoutes = require('./routes/users');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const PORT = 3000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const Gift = require("./models/gift");
+const Group = require("./models/group");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const userRoutes = require("./routes/users");
+const groups = require("./routes/groups");
+const gifts = require("./routes/gifts");
 
-const groups = require('./routes/groups');
-const gifts = require('./routes/gifts');
-
-mongoose.connect('mongodb://localhost:27017/localGift', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const mongodb_url = process.env.MONGODB_URL;
+// "mongodb://localhost:27017/localGift"
+mongoose.connect(mongodb_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "console error:"));
 db.once("open", () => {
-    console.log("Database connected")
-})
+  console.log("Database connected");
+});
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.engine('ejs', ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 // in order to parse the body in a post request
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionOp = {
-    secret: 'thisthisthis',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 app.use(session(sessionOp));
 app.use(flash());
 
@@ -57,20 +61,20 @@ passport.serializeUser(User.serializeUser()); //this brings a user in a session
 passport.deserializeUser(User.deserializeUser()); //this brings a user out of a session
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-app.use('/', userRoutes);
-app.use('/groups', groups);
-app.use('/groups/:id/gifts', gifts);
+app.use("/", userRoutes);
+app.use("/groups", groups);
+app.use("/groups/:id/gifts", gifts);
 
 app.use((req, res) => {
-    res.status(404).send('Page Not Found!')
-})
+  res.status(404).send("Page Not Found!");
+});
 
 app.listen(PORT, () => {
-    console.log(`Serving on PORT ${PORT}`)
-})
+  console.log(`Serving on PORT ${PORT}`);
+});
--- routes/gifts.js
+++ routes/gifts.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router({ mergeParams: true }); // mergeparams to merge params from app.js into this file so that req.params.id is not null
-const Group = require('../models/group');
-const Gift = require('../models/gift');
-const { isLoggedIn, isAuthor } = require('../middleware');
+const Group = require("../models/group");
+const Gift = require("../models/gift");
+const { isLoggedIn, isAuthor } = require("../middleware");
 
 // This post method will add a new gift to corresponding group
-router.post('/', isLoggedIn, isAuthor, async(req, res) => {
-    const group = await Group.findById(req.params.id);
-    const gift = new Gift(req.body.gift);
-    gift.author = req.user._id;
-    group.gifts.push(gift)
-    await gift.save();
-    await group.save();
-    req.flash('success', 'New gift added to wishlist!')
-    res.redirect(`/groups/${group._id}`);
+router.post("/", isLoggedIn, isAuthor, async (req, res) => {
+  const group = await Group.findById(req.params.id);
+  const gift = new Gift(req.body.gift);
+  gift.author = req.user._id;
+  group.gifts.push(gift);
+  await gift.save();
+  await group.save();
+  req.flash("success", "New gift added to wishlist!");
+  res.redirect(`/groups/${group._id}`);
 });
 
+// router.post('/', isLoggedIn, isGroup, async(req, res) => {
+
+// })
+
 // This delete method allows users to delete their gifts from their wishlist
-router.delete('/:giftId', isLoggedIn, isAuthor, async(req, res) => {
-    const { id, giftId } = req.params
-    await Group.findByIdAndUpdate(id, {$pull: { gifts: giftId }});
-    await Gift.findByIdAndDelete(req.params.giftId);
-    req.flash('success', 'Gift successfully removed from wishlist!')
-    res.redirect(`/groups/${id}`);
+router.delete("/:giftId", isLoggedIn, isAuthor, async (req, res) => {
+  const { id, giftId } = req.params;
+  await Group.findByIdAndUpdate(id, { $pull: { gifts: giftId } });
+  await Gift.findByIdAndDelete(req.params.giftId);
+  req.flash("success", "Gift successfully removed from wishlist!");
+  res.redirect(`/groups/${id}`);
 });
 
 module.exports = router;
